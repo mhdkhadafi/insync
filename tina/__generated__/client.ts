bd895e391dab4b8a2af7203822f4dc8b212661e5
Muhammad Khadafi <dafidafidafidafidafi@Muhammads-MacBook-Pro.local>
2026-03-10 01:33:35 -0400

    feat: replace Tina CMS with Decap CMS + Netlify Identity

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/dafidafidafidafidafi/Documents/Projects/insync/tina/__generated__/.cache/1773115572999', url: 'https://content.tinajs.io/2.2/content/dummy/github/main', token: 'dummy', queries,  });
-export default client;
-  
